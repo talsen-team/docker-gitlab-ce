5c878157d986d28e0965b6e23a89d7f6b3f04a37
David Lechner <david.lechner@talsen.team>
2018-09-05 09:33:12 +0200

    bash-docker command update (delete-volumes fix skipped removal of volumes)

--- bash-docker/command-delete-volumes.sh
+++ bash-docker/command-delete-volumes.sh
@@ -13,7 +13,7 @@ VAR_RESULT="";
 if [ -z "${ENV_SERVICES_DEFAULT_VOLUMES_ROOT}" ]; then
   VAR_RESULT="$(TextColorYellow)skipped$(TextColorDefault)"
 else
-  rm -rf "${ENV_SERVICES_DEFAULT_VOLUMES_ROOT}/*"
+  rm -rf "${ENV_SERVICES_DEFAULT_VOLUMES_ROOT}"
   VAR_RESULT="$(TextColorGreen)done$(TextColorDefault)"
 fi
 
