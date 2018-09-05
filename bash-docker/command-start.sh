cabd386c0d303b32a82cd1e0ab1759ebb451ca07
David Lechner <david.lechner@talsen.team>
2018-09-05 09:14:21 +0200

    bash-docker command update (remove unused code)

--- bash-docker/command-start.sh
+++ bash-docker/command-start.sh
@@ -6,6 +6,4 @@ source ./util-functions.sh
 
 PrepareDockerComposeEnvironment ${@}
 
-export ENV_SERVICES_SERVER_MATRIX_SYNAPSE_COMMAND=start
-
 docker-compose --file ${ENV_PATH_FILE_DOCKER_COMPOSE} start
